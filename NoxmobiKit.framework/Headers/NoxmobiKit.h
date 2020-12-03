b8c4e6fb79cc102434405528d2ed5d420dbb44a5
nox_chong <gaochong@bignox.com>
2020-12-03 17:52:47 +0800

    v1.1.1

--- NoxmobiKit.framework/Headers/NoxmobiKit.h
+++ NoxmobiKit.framework/Headers/NoxmobiKit.h
@@ -9,7 +9,7 @@
 
 #import <Foundation/Foundation.h>
 
-#define NOXMOBI_KIT_VER @"1.1.0"
+#define NOXMOBI_KIT_VER @"1.1.1"
 
 // In this header, you should import all the public headers of your framework using statements like #import <NoxmobiKit/PublicHeader.h>
 
