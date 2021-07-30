b3026f0f3cb1ab8996688183b4d17ede5db0fe1c
nox_chong <gaochong@bignox.com>
2021-07-30 11:27:47 +0800

    v1.3.0

--- gitHelper.sh
+++ gitHelper.sh
@@ -6,6 +6,13 @@ echo "\n🔥当前版本号："
 srcVer=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" ./$SDKName.framework/Info.plist)
 echo "\n🔥版本号 = " "$srcVer"
 
+echo "\n🔥是否继续执行?[y/n]"
+read flag
+if [[ "$flag" != "y" ]]
+then
+    exit 101
+fi
+
 echo "\n🔥查看改动 git status："
 gitStatusRes=`git status`
 echo "$gitStatusRes"
@@ -20,3 +27,10 @@ echo "$gitCommitRes"
 echo "\n🔥推送到远程 git push："
 gitPushRes=`git push origin main`
 echo "$gitPushRes"
+
+echo "\n🔥打本地标签 git tag："
+gitTagRes=`git tag $srcVer`
+
+echo "\n🔥推送标签 git tag："
+gitPushTagRes=`git push origin $srcVer`
+echo "$gitPushTagRes"
